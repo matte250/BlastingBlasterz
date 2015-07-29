8faf1422403b2767e3aaf914d81308ace3080a8d
matte250 <matte_1500@hotmail.com>
2015-07-29 16:42:17 +0200

    Test Commit 1
    Yes indeed test.

--- core/src/com/rosoccos/blasterz/Blasterz.java
+++ core/src/com/rosoccos/blasterz/Blasterz.java
@@ -8,12 +8,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Blasterz extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
 	}
 
 	@Override
@@ -21,7 +21,7 @@ public class Blasterz extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
 		batch.end();
 	}
 }
--- desktop/src/com/rosoccos/blasterz/desktop/DesktopLauncher.java
+++ desktop/src/com/rosoccos/blasterz/desktop/DesktopLauncher.java
@@ -8,5 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new Blasterz(), config);
+		config.resizable = false;
+		config.height = 100;
 	}
 }
