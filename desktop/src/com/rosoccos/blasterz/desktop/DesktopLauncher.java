9f0f103524a02f78d07989b112e4b52331f91c50
matte250 <matte_1500@hotmail.com>
2015-07-29 16:26:51 +0200

    Testcommit
    Yes.

---
+++ desktop/src/com/rosoccos/blasterz/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rosoccos.blasterz.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rosoccos.blasterz.Blasterz;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Blasterz(), config);
+	}
+}
