8050062244a42ded227104f35fa980d59f1e68bb
matte250 <matte_1500@hotmail.com>
2015-07-29 16:43:57 +0200

    Test 2

--- core/src/com/rosoccos/blasterz/Blasterz.java
+++ core/src/com/rosoccos/blasterz/Blasterz.java
@@ -18,7 +18,7 @@ public class Blasterz extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 
